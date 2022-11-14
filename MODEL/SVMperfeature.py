02616caeaeea8b51d11c22be959476f409a0936a
rene-quesada <rene.quesada@ucr.ac.cr>
2022-11-13 22:13:34 -0600

    agregando codigo para correr svm para cada uno de los features

--- MODEL/SVMperfeature.py
+++ MODEL/SVMperfeature.py
@@ -60,70 +60,73 @@ def train_with_data(top_n_features, data_obj):
 
     df = df_benign.append(df_mal)
 
-    #get all features
-    col_names = df.columns    
 
-    #setup data for training
-    Y = df['malicious']
-    X = df.drop(['malicious'],axis = 1)
+    for column in df:
+        if column == 'malicious':
+            continue
+
+        #setup data for training
+        Y = df['malicious']
+        X = df[column]
+        
+        
+        X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.20, random_state=42)
+
+        scaler = StandardScaler()
+        X_train.shape, X_test.shape
+        X_train = np.array(X_train).reshape(-1, 1)
+        X_test = np.array(X_test).reshape(-1, 1)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        result_name = 'MODEL/output/SVM_' + column
+        results_path_png = result_name + '.png'
+        results_path_txt = result_name + '.txt'
+        
+        with open(results_path_txt, 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            print('The number of records in the training dataset is', X_train.shape[0])
+            print('The number of records in the test dataset is', X_test.shape[0])
+            print(f"The training dataset has {sorted(Counter(y_train).items())[0][1]} records for the majority class and {sorted(Counter(y_train).items())[1][1]} records for the minority class.")
+            sys.stdout = original_stdout # Reset the standard output to its original value
+
+        #create SVC model
+        svc=SVC(C=100.0, kernel = 'rbf',gamma= 0.1) 
+        
+        #train the model
+        svc.fit(X_train,y_train)
+
+        prediction = svc.predict(X_test)
+
+        # compute and print accuracy score
+        with open(results_path_txt, 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            print('Model accuracy score with default hyperparameters: {0:0.4f}'. format(accuracy_score(y_test, prediction)))
+            sys.stdout = original_stdout # Reset the standard output to its original value  
+
+        cm = confusion_matrix(y_test, prediction)
+
+        with open(results_path_txt, 'a') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            print('Confusion matrix\n\n', cm)
+            print('\nTrue Positives(TP) = ', cm[0,0])
+            print('\nTrue Negatives(TN) = ', cm[1,1])
+            print('\nFalse Positives(FP) = ', cm[0,1])
+            print('\nFalse Negatives(FN) = ', cm[1,0])
+            sys.stdout = original_stdout # Reset the standard output to its original value
+
+        cm_matrix = pd.DataFrame(data=cm, columns=['Actual Positive:1', 'Actual Negative:0'], 
+                                    index=['Predict Positive:1', 'Predict Negative:0'])
+
+        sns_plot = sns.heatmap(cm_matrix, annot=True, fmt='d', cmap='YlGnBu')
+
+        plt.savefig(results_path_png, dpi=400)
+
+        with open(results_path_txt, 'a') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            print(classification_report(y_test, prediction))
+            sys.stdout = original_stdout # Reset the standard output to its original value    
     
-    
-    
-    X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.20, random_state=42)
-
-    scaler = StandardScaler()
-    X_train.shape, X_test.shape
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    result_name = 'MODEL/output/SVM_All_features'
-    results_path_png = result_name + '.png'
-    results_path_txt = result_name + '.txt'
-    
-    with open(results_path_txt, 'w') as f:
-        sys.stdout = f # Change the standard output to the file we created.
-        print('The number of records in the training dataset is', X_train.shape[0])
-        print('The number of records in the test dataset is', X_test.shape[0])
-        print(f"The training dataset has {sorted(Counter(y_train).items())[0][1]} records for the majority class and {sorted(Counter(y_train).items())[1][1]} records for the minority class.")
-        sys.stdout = original_stdout # Reset the standard output to its original value
-
-    #create SVC model
-    svc=SVC(C=100.0, kernel = 'rbf',gamma= 0.1) 
-    
-    #train the model
-    svc.fit(X_train,y_train)
-
-    prediction = svc.predict(X_test)
-
-    # compute and print accuracy score
-    with open(results_path_txt, 'w') as f:
-        sys.stdout = f # Change the standard output to the file we created.
-        print('Model accuracy score with default hyperparameters: {0:0.4f}'. format(accuracy_score(y_test, prediction)))
-        sys.stdout = original_stdout # Reset the standard output to its original value  
-
-    cm = confusion_matrix(y_test, prediction)
-
-    with open(results_path_txt, 'a') as f:
-        sys.stdout = f # Change the standard output to the file we created.
-        print('Confusion matrix\n\n', cm)
-        print('\nTrue Positives(TP) = ', cm[0,0])
-        print('\nTrue Negatives(TN) = ', cm[1,1])
-        print('\nFalse Positives(FP) = ', cm[0,1])
-        print('\nFalse Negatives(FN) = ', cm[1,0])
-        sys.stdout = original_stdout # Reset the standard output to its original value
-
-    cm_matrix = pd.DataFrame(data=cm, columns=['Actual Positive:1', 'Actual Negative:0'], 
-                                 index=['Predict Positive:1', 'Predict Negative:0'])
-
-    sns_plot = sns.heatmap(cm_matrix, annot=True, fmt='d', cmap='YlGnBu')
-
-    plt.savefig(results_path_png, dpi=400)
-
-    with open(results_path_txt, 'a') as f:
-        sys.stdout = f # Change the standard output to the file we created.
-        print(classification_report(y_test, prediction))
-        sys.stdout = original_stdout # Reset the standard output to its original value    
-  
 
 if __name__ == '__main__':
     train(*sys.argv[1:])
